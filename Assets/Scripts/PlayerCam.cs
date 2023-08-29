379f40323cb4cf56c07257b3863fc979044e9a55
Philip Pesic <116986036+philippesic@users.noreply.github.com>
2023-08-28 17:35:41 -0700

    Project setup and player/cam movement

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    float xRotation;
+    float yRotation;
+
+    [Header("Sensitivity")]
+    public float sensX;
+    public float sensY;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+    }
+}
