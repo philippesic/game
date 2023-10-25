74cad4c069f2559439fbc120d19a581f13f15d2c
Philip Pesic <116986036+philippesic@users.noreply.github.com>
2023-10-25 10:26:34 -0700

    i added them 😃

--- Assets/Scripts/AllGameData.cs
+++ Assets/Scripts/AllGameData.cs
@@ -144,6 +144,8 @@ public class AllGameData : ScriptableObject
         itemNames.Add(id, name);
         itemDescriptions.Add(id, description);
         itemIcons.Add(id, Resources.Load<Sprite>("Items/Icons/" + itemNames[id]));
+        Debug.Log(itemNames[id] + " " + id);
+        Debug.Log(itemIcons[id]);
         itemPrefabs.Add(id, Resources.Load<GameObject>("Items/Prefabs/" + itemNames[id]));
     }
 
