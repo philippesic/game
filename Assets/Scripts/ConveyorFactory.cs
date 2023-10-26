eb0cda484346c387017a64c5a67e51e9be7dab85
Ben Herman <bherman24@dtechhs.org>
2023-10-26 11:32:22 -0700

    removed prints

--- Assets/Scripts/ConveyorFactory.cs
+++ Assets/Scripts/ConveyorFactory.cs
@@ -19,7 +19,6 @@ public class ConveyorFactory : ItemObjectContainingFactory
 
     public override bool CanPush(ItemObjectContainingFactory containingFactory)
     {
-        Debug.Log("CanPush");
         if (containingFactory != null)
         {
             return containingFactory.HasRoomToPush();
@@ -29,16 +28,13 @@ public class ConveyorFactory : ItemObjectContainingFactory
 
     public override bool HasRoomToPush()
     {
-        Debug.Log("HasRoomToPush");
         return heldItem == null;
     }
 
     public override void Tick()
     {
-        Debug.Log("Tick");
         if (neighborFactories.ContainsKey("(0, 0, 1)"))
         {
-            Debug.Log(heldItem);
             ItemObjectContainingFactory containingFactory = neighborFactories["(0, 0, 1)"].GetBlockFromType<ItemObjectContainingFactory>();
             if (heldItem == null)
             {
@@ -46,7 +42,6 @@ public class ConveyorFactory : ItemObjectContainingFactory
             }
             else if (shouldMoveItems && CanPush(containingFactory))
             {
-                Debug.Log("Tick3");
                 shouldMoveItems = false;
                 containingFactory.GiveItem(heldItem);
                 RemoveItem(heldItem);
