372bdd2b1056cedefd946c6b0614b4ff080167aa
Philip Pesic <116986036+philippesic@users.noreply.github.com>
2023-11-13 10:10:45 -0800

    eriub

--- Assets/Scripts/ShopPlace.cs
+++ Assets/Scripts/ShopPlace.cs
@@ -7,7 +7,20 @@ using UnityEditor;
 public class ShopPlace : UI
 {
     private WorldBlockPlacer worldBlockPlacer;
-    public GameObject player;
+    private ShopController shop;
+
+
+    void Start()
+    {
+        worldBlockPlacer = gameObject.GetComponentInChildren<WorldBlockPlacer>();
+        shop = gameObject.GetComponentInParent<ShopController>();
+    }
+
+    public void PlaceFactory(int id)
+    {
+        shop.ToggleShop();
+        worldBlockPlacer.StartPlacement(id);
+    }
 
     //This script is attatched to the shop. INstance worldblock placer and do the stuff to instance placing a factory.
     //Each button assign a place function with an id parameter to place correct object
